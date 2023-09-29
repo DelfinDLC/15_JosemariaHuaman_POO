3762c725dbc0a9f23b188a665e1e16788606aceb
DelfinDLC <138272140+DelfinDLC@users.noreply.github.com>
2023-09-29 13:55:15 -0500

    Estudiante_11.java

--- Estudiante_11.java
+++ Estudiante_11.java
@@ -9,6 +9,24 @@ package S05_11JosemariaHuaman;
  *
  * @author josem
  */
+import javax.swing.JOptionPane;
+
+
+
 public class Estudiante_11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String estudiante;
+        int edad;
+       
+        estudiante=JOptionPane.showInputDialog("Nombre del Estudiante");
+        edad = Integer.parseInt(JOptionPane.showInputDialog("Edad del Estudiante"));
+        JOptionPane.showMessageDialog(null, "Información almacenada: "+ estudiante+" "+edad);
+    }
     
 }
